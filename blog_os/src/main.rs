5ce1f569195cec837761273cb62a2dcf130539a7
prisar <pritimaysarkar2010@gmail.com>
2022-05-23 22:58:15 +0530

    vga

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
@@ -13,6 +15,9 @@ pub extern "C" fn _start() -> ! {
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
+
+
+    vga_buffer::print_something();
 
     loop {}
 }
