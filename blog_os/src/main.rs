c18b7e78004870dfff0d58cc05d2250525cbe94e
prisar <pritimaysarkar2010@gmail.com>
2022-05-24 14:57:19 +0530

    spinlock

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -4,21 +4,11 @@
 mod vga_buffer;
 
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop {}
 }
@@ -30,4 +20,5 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
 
--- blog_os/src/vga_buffer.rs
+++ blog_os/src/vga_buffer.rs
@@ -135,13 +135,29 @@ pub fn print_something() {
 }
 
 use lazy_static::lazy_static;
+use spin::Mutex;
 
 lazy_static! { 
-    pub static ref WRITER: Writer = Writer {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
     	color_code: ColorCode::new(Color::Yellow, Color::Black),
     	buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+    });
 }
 
+#[macro_export]
+macro_rules! println {
+    () => (print!("\n"));
+    ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
+}
 
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::io::_print(format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
