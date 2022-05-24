859d71ddb4469d0f6c09fa0670d2b16c95ece555
prisar <pritimaysarkar2010@gmail.com>
2022-05-24 15:32:12 +0530

    test

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,13 +1,27 @@
 #![no_std]
 #![no_main]
 
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -22,4 +36,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
