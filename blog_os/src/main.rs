7f464aea2cd55cc9a0434deebb415a9c0e88cc11
prisar <pritimaysarkar2010@gmail.com>
2022-05-25 19:29:10 +0530

    doublt fault

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -14,8 +14,11 @@ pub extern "C" fn _start() -> ! {
 
     blog_os::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
+
 
     #[cfg(test)]
     test_main();
