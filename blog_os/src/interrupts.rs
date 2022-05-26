af6691564fd9bc73288318552a76fb12d02322c6
prisar <pritimaysarkar2010@gmail.com>
2022-05-26 14:13:24 +0530

    page fault

--- blog_os/src/interrupts.rs
+++ blog_os/src/interrupts.rs
@@ -18,16 +18,18 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-	unsafe {
-	    idt.double_fault.set_handler_fn(double_fault_handler)
-        	.set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); 
-	}
+//	unsafe {
+//	    idt.double_fault.set_handler_fn(double_fault_handler)
+//        	.set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); 
+//	}
 	
 	idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);
 
 	idt[InterruptIndex::Keyboard.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
+
+	idt.page_fault.set_handler_fn(page_fault_handler);
 
 	idt
     };
@@ -115,4 +117,20 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
+}
+
+use x86_64::structures::idt::PageFaultErrorCode;
+use crate::hlt_loop;
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
