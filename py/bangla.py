95029bf43697ecb4c9106df34443cfe7ce2ce390
prisar <prisar230@gmail.com>
2023-01-16 12:19:55 +0530

    token

---
+++ py/bangla.py
@@ -0,0 +1,7 @@
+from transformers import AutoTokenizer, AutoModel
+
+bnbert_tokenizer = AutoTokenizer.from_pretrained("sagorsarker/bangla-bert-base")
+text = "আমি বাংলায় গান গাই।"
+tokens = bnbert_tokenizer.tokenize(text)
+print(tokens)
+# ['আমি', 'বাংলা', '##য', 'গান', 'গাই', '।']
